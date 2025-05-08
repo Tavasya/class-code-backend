56d144fc5c80aaeb9eaf012051768b585b728002
Tavasya Ganpati <tavasya@Tavasyas-MacBook-Pro.local>
2025-05-08 10:28:50 -0700

    g

--- app.py
+++ app.py
@@ -909,7 +909,7 @@ async def test_transcription_length(url: str):
         
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8081))
-    uvicorn.run(app, host="0.0.0.0", port=8081)
+    uvicorn.run(app, host="0.0.0.0", port=port)
     
 
 
