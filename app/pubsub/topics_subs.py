a2618b21bd08b59c9100033db329e87bcb13ec8e
Tavasya Ganpati <tavasya@Tavasyas-MacBook-Pro.local>
2025-05-22 17:46:13 -0700

    fixed topic subs

--- app/pubsub/topics_subs.py
+++ app/pubsub/topics_subs.py
@@ -16,12 +16,12 @@ TOPICS: Dict[str, str] = {
 # Subscription Names - Define all available subscriptions here
 SUBSCRIPTIONS: Dict[str, str] = {
     "ANALYSIS_COMPLETE": "analysis-complete-topic-sub",
-    "AUDIO_CONVERSION_DONE": "audio-conversion-done-topic-sub",
+    "AUDIO_CONVERSION_DONE": "audio-conversion-service-sub",
     "FLUENCY_DONE": "fluency-done-topic-sub",
     "GRAMMER_DONE": "grammer-done-topic-sub",
     "LEXICAL_DONE": "lexical-done-topic-sub",
     "PRONOUN_DONE": "pronoun-done-topic-sub",
     "QUESTION_ANALYSIS_READY": "question-analysis-ready-topic-sub",
     "STUDENT_SUBMISSION": "student-submission-topic-sub",
-    "TRANSCRIPTION_DONE": "transcription-done-topic-sub"
+    "TRANSCRIPTION_DONE": "transcription-service-sub"
 }
