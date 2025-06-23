90fa49d2631fc3581d258e8fd197b39176292fc2
Tavasya Ganpati <tavasya@Tavasyas-MacBook-Pro.local>
2025-06-23 12:15:40 -0700

    added cap of 70 for pro

--- app/services/pronunciation_service.py
+++ app/services/pronunciation_service.py
@@ -438,6 +438,10 @@ class PronunciationService:
                 assessment = word.get("PronunciationAssessment", {})
                 accuracy_score = assessment.get("AccuracyScore", 0)
                 
+                # Skip words with accuracy score over 70 (don't add to JSON)
+                if accuracy_score > 70:
+                    continue
+                
                 # Convert timings from 100-nanosecond units to seconds
                 offset_seconds = word.get("Offset", 0) / 10000000
                 duration_seconds = word.get("Duration", 0) / 10000000
